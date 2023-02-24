820cc098b3f09fa936ab844b2ea6a4ded3c95bfe
wxwx1993 <wuxiao199301@126.com>
2023-02-24 15:20:56 -0500

    Update process_analysis_data.R

--- data_processing/process_analysis_data.R
+++ data_processing/process_analysis_data.R
@@ -1,85 +1,108 @@
+## Process data for the covariate balance analysis
+## combine active fire, pre treatment historical fire, climate, land cover data into single df
+## the df is per exposure year (2008-2020) per land type (forests, savannas, grasslands)
 print(Sys.time())
 rm(list = ls())
-library(MatchIt)
-library(fst)
 library("sf")
 library("tidyverse")
-library("RCAL")
-library("CVXR")
-library("tigris")
 library("mltools")
 library("data.table")
 
+outDir <- "../data/processed_data"
 
-outDir = "../data/processed_data"
+#year up to 2020
+parameters <- expand.grid(c(2005:2020), c("forests", "savannas", "grasslands"))
+# year_area = 1                          
+for (year_area in 1:nrow(parameters)) {
+  treated.year <- as.numeric(parameters[year_area, 1])
+  area <- as.character(parameters[year_area, 2])
+  
+  # choose only pre-exposure data (i.e., for exposure year XXXX, only covariates before that year will be chosen)
+  col.idx2 <- c(1, 2, as.numeric(outer(c(3:(2 + 12 * (treated.year - 2000))), seq(0, 4, 1) * (21*12), FUN = "+")))
+  col.idx3 <- c(1, 2, as.numeric(outer(c(3:(2 + (treated.year - 2000))), seq(0, 2, 1) * (22), FUN = "+")))
+  col.idx4 <- c(1, 2, as.numeric(outer(c(3:(2 + (treated.year - 2000))), seq(3, 3, 1) * (20), FUN = "+")))
 
-#year up to
-treated.year = 2008
-area = "grasslands"
+  landcover <- readRDS(file.path(outDir, "LandCover.RDS"))
+  landcover <- st_as_sf(landcover,
+                        coords = c("LONGITUDE", "LATITUDE"),
+                        crs = 4326,
+                        remove = FALSE)
+  st_geometry(landcover) <- NULL
+  landcover <- landcover[col.idx4]
+  if (area == "forests") {
+    landcover <- landcover %>%
+      filter_at(c(c(3:(2 + (treated.year - 2000)))), all_vars(. <= 5))
+  } else if (area == "savannas") {
+    landcover <- landcover %>%
+      filter_at(c(c(3:(2 + (treated.year - 2000)))), all_vars(. >= 8 & . <= 9))
+  } else if (area == "grasslands") {
+    landcover <- landcover %>%
+      filter_at(c(c(3:(2 + (treated.year - 2000)))), all_vars(. == 10))
+  }
+  landcover[,c(3:(2 + (treated.year-2000)))] <- lapply(landcover[, c(3:(2 + (treated.year - 2000)))], factor)
+  landcover <- cbind(landcover[,c(1:2)], one_hot(as.data.table(landcover[,c(3:(2 + (treated.year - 2000)))])))
+  landcover$unit <- paste0(landcover$LATITUDE, landcover$LONGITUDE)
+  landcover$LATITUDE <- NULL
+  landcover$LONGITUDE <- NULL
 
-#col.idx = c(1,2, as.numeric(outer(c(3:(2 + 12*11)), seq(0, 15, 1) * (22*12), FUN = "+")))
-#df = df[col.idx]
-col.idx2 = c(1,2, as.numeric(outer(c(3:(2 + 12*(treated.year-2000))), seq(0, 4, 1) * (21*12), FUN = "+")))
-col.idx3 = c(1,2, as.numeric(outer(c(3:(2 + 12*(treated.year-2000))), seq(0, 2, 1) * (22*12), FUN = "+")))
-col.idx4 = c(1,2, as.numeric(outer(c(3:(2 + (treated.year-2000))), seq(3, 3, 1) * (20), FUN = "+")))
+  df <- read.fst(file.path(outDir, "gridClimate_mon.fst"),
+                 from = 1,
+                 to = NULL,
+  )
+  df <- df[col.idx2]
+  
+  fire_brightness_frp <- read.fst(file.path(outDir, "fire_brightness_frp.fst"),
+                                  from = 1,
+                                  to = NULL,
+  )
+  fire_brightness_frp <- fire_brightness_frp[col.idx3]
 
-landcover <- readRDS(file.path(outDir, "LandCover.RDS"))
-landcover <- st_as_sf(landcover,
-                      coords = c("LONGITUDE", "LATITUDE"),
-                      crs = 4326,
-                      remove = FALSE)
-st_geometry(landcover) <- NULL
-landcover <- landcover[col.idx4]
-landcover <- landcover %>%
-#  filter_at(c(c(3:(2 + (treated.year-2000)))), all_vars(. <= 5))
-#  filter_at(c(c(3:(2 + (treated.year-2000)))), all_vars(. >= 6 & . <= 7))
-#  filter_at(c(c(3:(2 + (treated.year-2000)))), all_vars(. >= 8 & . <= 9))
-  filter_at(c(c(3:(2 + (treated.year-2000)))), all_vars(. == 10))
-landcover[,c(3:(2 + (treated.year-2000)))] <- lapply(landcover[,c(3:(2 + (treated.year-2000)))] , factor)
-landcover <- cbind(landcover[,c(1:2)], one_hot(as.data.table(landcover[,c(3:(2 + (treated.year-2000)))])))
-#df = cbind(df2, landcover[,3:ncol(landcover)])
-landcover$unit = paste0(landcover$LATITUDE, landcover$LONGITUDE)
-landcover$LATITUDE <- NULL
-landcover$LONGITUDE <- NULL
+  df <- cbind(df, fire_brightness_frp[,3:ncol(fire_brightness_frp)])
 
-df = read.fst(file.path(outDir, "gridClimate_mon.fst"),
-               from = 1,
-               to = NULL,
-)
-df = df[col.idx2]
+  unit <- paste0(df$LATITUDE, df$LONGITUDE)
+  df$unit <- unit
 
-fire_brightness_frp = read.fst(file.path(outDir, "fire_brightness_frp.fst"),
-                               from = 1,
-                               to = NULL,
-)
+  FIRMS_ca_grouped = readRDS(file.path(outDir, "FIRMS.RDS"))
+  FIRMS_ca_grouped$unit = paste0(FIRMS_ca_grouped$LATITUDE, FIRMS_ca_grouped$LONGITUDE)
+  st_geometry(FIRMS_ca_grouped) <- NULL
+  fire.df <- subset(FIRMS_ca_grouped, year == treated.year)
+  fire.index = unique(fire.df[c("unit")])
 
-fire_brightness_frp = fire_brightness_frp[col.idx3]
+  hist.fire.df <- subset(FIRMS_ca_grouped, year <= treated.year - 1)
+  hist.fire.df$has.fire = 1
+  hist.fire.df <- hist.fire.df %>%
+    group_by(unit) %>%
+    summarise(num.fire = sum(has.fire))
+  
+  # Keep only covariates and treatment
+  df <- merge(landcover, df, by = "unit", all.x = TRUE)
 
-df = cbind(df, fire_brightness_frp[,3:ncol(fire_brightness_frp)])
-
-unit = paste0(df$LATITUDE, df$LONGITUDE)
-df$unit = unit
-
-FIRMS_ca_grouped = readRDS(file.path(outDir, "FIRMS.RDS"))
-FIRMS_ca_grouped$unit = paste0(FIRMS_ca_grouped$LATITUDE, FIRMS_ca_grouped$LONGITUDE)
-st_geometry(FIRMS_ca_grouped) <- NULL
-fire.df <- subset(FIRMS_ca_grouped, year == treated.year)
-fire.df = unique(fire.df[c("unit")])
-
-hist.fire.df <- subset(FIRMS_ca_grouped, year <= treated.year - 1)
-hist.fire.df$has.fire = 1
-hist.fire.df <- hist.fire.df %>%
-  group_by(unit) %>%
-  summarise(num.fire = sum(has.fire))
-
-df <- merge(df, hist.fire.df, by = "unit", all.x = TRUE)
-df$num.fire[is.na(df$num.fire)] <- 0
-# treated = all units which had fire in treated.year
-df$treated = 0
-df[df$unit %in% fire.df$unit, "treated"] = 1
-# Keep only covariates and treatment
-df <- merge(landcover, df, by = "unit", all.x = TRUE)
-
-saveRDS(df, file = paste0("analysis_treated", treated.year, "_", area, ".RDS"))
-
-
+  df <- merge(df, hist.fire.df, by = "unit", all.x = TRUE)
+  df$num.fire[is.na(df$num.fire)] <- 0
+  # treated = all units which had fire in treated.year
+  df$treated = 0
+  df[df$unit %in% fire.index$unit, "treated"] = 1
+  
+  fire.df_year <- fire.df %>% 
+    group_by(LATITUDE, LONGITUDE, year) %>% 
+    summarise(max_FRP = max(max_FRP))
+  
+  # classify the fire types by max FRP for each years
+  # based on systems proposed in https://www.sciencedirect.com/science/article/abs/pii/S003442570800062X
+  fire.df_year$class <- 0 
+  fire.df_year[fire.df_year$max_FRP == 0,]$class <- 0
+  fire.df_year[0 < fire.df_year$max_FRP & fire.df_year$max_FRP < 100,]$class <- 1
+  fire.df_year[100 <= fire.df_year$max_FRP & fire.df_year$max_FRP < 500,]$class <- 2
+  fire.df_year[500 <= fire.df_year$max_FRP & fire.df_year$max_FRP < 1000,]$class <- 3
+  fire.df_year[1000 <= fire.df_year$max_FRP & fire.df_year$max_FRP < 1500,]$class <- 4
+  fire.df_year[fire.df_year$max_FRP >= 1500,]$class <- 5
+  
+  saveRDS(fire.df_year, file = paste0(outDir, "/analysis_low/fire_class/fire.df", treated.year, "_", area, ".RDS"))
+  
+  # df for the covariate balance analysis, only keep low intensity fire class 1
+  fire.df$has.hifire <- 0
+  fire.df[fire.df$max_FRP >= 100,]$has.hifire <- 1
+  df <- subset(df, !(unit %in% subset(fire.df, has.hifire == 1)$unit))
+  
+  saveRDS(df, file = file.path(outDir, "analysis_low", paste0("analysis_treated", treated.year, "_", area, ".RDS")))
+}
